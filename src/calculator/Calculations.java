8e38e910a45af863029d1c9b07cc7bdb2ece6d44
Julias Biswas <juliasbiswas0001@gmail.com>
2022-04-12 16:28:27 +0530

    Initial commit

---
+++ src/calculator/Calculations.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author julia
+ */
+public interface Calculations 
+{
+    
+    void add(double a , double b);
+    
+    void substract(double a , double b);
+    
+    void multiply(double a , double b);
+    
+    void divide(double a , double b);
+ 
+}
